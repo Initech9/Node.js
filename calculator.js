c35bf6d6a14d04209861fd53b3408127bf1cb5fb
Initech9 <ag9876@gmail.com>
2020-04-20 01:00:17 -0400

    Text Database
    Added text file database

--- calculator.js
+++ calculator.js
@@ -5,11 +5,23 @@ const app = express();
 var fs = require('fs');
 var os = require("os");
 
+var fileData;
+
 app.use(parser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
+
+
+
+    fs.readFile('mynewfile1.txt', 'utf8', function(err, data) {
+        if (err) throw err;
+        console.log(data);
+        fileData = data;
+    });
+
 });
+
 
 app.post("/", function(req, res){
 
@@ -21,10 +33,22 @@ var result = num1 + num2;
     
 res.send("The two numbers added are " + result);
 
+
+//res.send(fileData);
+
 fs.appendFile('mynewfile1.txt', result + os.EOL + name1 + os.EOL, function (err) {
     if (err) throw err;
     console.log('Saved!');
   }); 
+});
+
+
+
+app.get("/mytextfile1",function(request, response){
+   
+    response.sendFile(__dirname + "/mynewfile1.txt");
+    //response.send("<textarea id='w3mission' rows='4' cols='50'>hahaha</textarea>");
+console.log(request);
 });
 
 app.get("/about",function(request, response){
